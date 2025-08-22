0108e537c36a46456f3442a90151f7a28962041f
sirkays <attehkayode2@gmail.com>
2025-08-22 18:03:10 +0100

    dockerized 1.5

--- texagonbackend/settings.py
+++ texagonbackend/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 if os.environ.get("LOCAL") == "0":
