a9bee4b17845855251c402812e5a6861806579d0
sirkays <attehkayode2@gmail.com>
2025-08-22 17:30:57 +0100

    dockerized 1.1

--- texagonbackend/settings.py
+++ texagonbackend/settings.py
@@ -117,9 +117,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
 
+if os.environ.get("LOCAL") == "0":
+    #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_URL = '/static/'        # <- needs the leading slash
+    STATIC_ROOT = '/app/staticfiles'  # <- exactly where you mounted the volume
+
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = '/app/media'  
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
