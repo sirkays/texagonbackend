be2580411865750c0804b3dd04b846f6ec2f8b16
sirkays <attehkayode2@gmail.com>
2025-08-22 18:07:17 +0100

    dockerized 1.6

--- texagonbackend/settings.py
+++ texagonbackend/settings.py
@@ -9,6 +9,15 @@ DEBUG = os.environ.get("DJANGO_DEBUG", "0") == "1"
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(",")
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://texagonbackend.esm.name.ng",
+]
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 
 
 # Application definition
