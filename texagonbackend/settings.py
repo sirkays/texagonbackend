5d9e3e695ff9d92acb7a9bab4aa171105b1a0e23
sirkays <attehkayode2@gmail.com>
2025-09-11 13:35:58 +0100

    just test

--- texagonbackend/settings.py
+++ texagonbackend/settings.py
@@ -132,8 +132,7 @@ USE_TZ = True
 
 TIME_ZONE= "Africa/Lagos"
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
